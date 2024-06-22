1e56bf4244f2b6442cd12df8fc1caa79e24c6742
Moussa-abakar-adam <abakarm974@gmail.com>
2024-06-22 17:59:38 +0200

    hello world

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+
+      Center(
+
+    child:  Text(
+    'Hello World',
+      style: TextStyle(fontSize: 50.2),
+      textDirection: TextDirection.ltr,
+
+    ),
+  )
+  );
+}
